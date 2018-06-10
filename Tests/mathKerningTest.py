adf805773e2c64855a87f6618c595f4e16fda047
Erik van Blokland <erik@letterror.com>
2018-06-10 22:33:08 +0200

    Remove egg Small change to kerningTest (which I will leave in place until fontMath is sorted)

--- Tests/mathKerningTest.py
+++ Tests/mathKerningTest.py
@@ -5,9 +5,10 @@ f = Font()
 f.groups["public.kern1.groupA"] = ['one', 'Bee']
 f.groups["public.kern2.groupB"] = ['two', 'Three']
 f.kerning[('public.kern1.groupA', 'public.kern2.groupB')] = -100
+f.kerning[('one', 'two')] = 0
 m = MathKerning(f.kerning, f.groups)
 
-print(m.groups())
-print((m*1.0).groups())
+print(m.items())
+print((m*1.0).items())
 
 
