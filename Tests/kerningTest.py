d5e614600b574804031fa563106c89f492e0a547
Erik van Blokland <erik@letterror.com>
2018-11-04 12:48:54 +0100

    build() needs Location objects, not dicts. This is really an issue for MutatorMath - but it's not so critical that this should wait.

--- Tests/kerningTest.py
+++ Tests/kerningTest.py
@@ -4,11 +4,11 @@ import fontMath.mathKerning
 from defcon.objects.font import Font
 from fontParts.fontshell import RFont
 from ufoProcessor.varModels import VariationModelMutator
-from mutatorMath.objects.mutator import buildMutator
+from mutatorMath.objects.mutator import buildMutator, Location
 from fontTools.designspaceLib import AxisDescriptor
 
 # kerning exception value. Different results for 1 and 0
-value = 0 
+value = 0
 
 #f = Font()
 f = RFont()		# doesn't make a difference
@@ -24,7 +24,7 @@ print("\tpair", ('public.kern1.groupA', 'two'), m[('public.kern1.groupA', 'two')
 print("\tpair", ('one', 'public.kern2.groupB'), m[('one', 'public.kern2.groupB')])
 print("\tpair", ('one', 'two'), m[('one', 'two')])
 
-items = [(dict(w=0), m), (dict(w=1), m)]
+items = [(Location(w=0), m), (Location(w=1), m)]
 a = AxisDescriptor()
 a.name = "w"
 a.minimum = 0
