8effadf9a44b3385dbc53ff061a7993636c00bea
Erik van Blokland <erik@letterror.com>
2019-02-02 14:15:16 +0100

    WIP - import really old rules - store muted sources in lib - conversion function - more tests (using non-public test files at the moment, sorry)

--- Lib/ufoProcessor/sp3.py
+++ Lib/ufoProcessor/sp3.py
@@ -1,4 +1,6 @@
 import os
+import glob
+
 from fontTools.misc.loggingTools import LogMixin
 from fontTools.designspaceLib import DesignSpaceDocument, AxisDescriptor, SourceDescriptor, RuleDescriptor, InstanceDescriptor
 
@@ -12,8 +14,47 @@ except ImportError:
 # For now I just want to migrate data out of Superpolator into designspace.
 # So not all data will migrate, just the stuff we can use. 
 
+"""
+
+
+  <lib>
+    <dict>
+      <key>com.letterror.skateboard.interactionSources</key>
+      <dict>
+        <key>horizontal</key>
+        <array/>
+        <key>ignore</key>
+        <array/>
+        <key>vertical</key>
+        <array/>
+      </dict>
+      <key>com.letterror.skateboard.mutedSources</key>
+      <array>
+        <array>
+          <string>IBM Plex Sans Condensed-Bold.ufo</string>
+          <string>foreground</string>
+        </array>
+      </array>
+      <key>com.letterror.skateboard.previewLocation</key>
+      <dict>
+        <key>weight</key>
+        <real>0.0</real>
+      </dict>
+      <key>com.letterror.skateboard.previewText</key>
+      <string>SKATE</string>
+    </dict>
+  </lib>
+
+
+
+"""
 
 superpolatorDataLibKey = "com.superpolator.data"    # lib key for Sp data in .designspace
+skateboardInteractionSourcesKey = "com.letterror.skateboard.interactionSources"
+skateboardMutedSourcesKey = "com.letterror.skateboard.mutedSources"
+skipExportKey = "public.skipExportGlyphs"
+skateboardPreviewLocationsKey = "com.letterror.skateboard.previewLocation"
+skateboardPreviewTextKey = "com.letterror.skateboard.previewText"
 
 class SuperpolatorReader(LogMixin):
     ruleDescriptorClass = RuleDescriptor
@@ -21,9 +62,11 @@ class SuperpolatorReader(LogMixin):
     sourceDescriptorClass = SourceDescriptor
     instanceDescriptorClass = InstanceDescriptor
 
-    def __init__(self, documentPath, documentObject):
+    def __init__(self, documentPath, documentObject, convertRules=True, convertData=True):
         self.path = documentPath
         self.documentObject = documentObject
+        self.convertRules = convertRules
+        self.convertData = convertData
         tree = ET.parse(self.path)
         self.root = tree.getroot()
         self.documentObject.formatVersion = self.root.attrib.get("format", "3.0")
@@ -39,18 +82,31 @@ class SuperpolatorReader(LogMixin):
 
     def read(self):
         self.readAxes()
-        self.readData()
-        self.readRules()
+        if self.convertData:
+            self.readData()
+        if self.convertRules:
+            self.readOldRules()
+            self.readSimpleRules()
         self.readSources()
         self.readInstances()
 
     def readData(self):
         # read superpolator specific data, view prefs etc.
+        # if possible convert it to skateboard
+        interactionSources = {'horizontal': [], 'vertical': [], 'ignore': []}
+        ignoreElements = self.root.findall(".ignore")
+        ignoreGlyphs = []
+        for ignoreElement in ignoreElements:
+            names = ignoreElement.attrib.get('glyphs')
+            if names:
+                ignoreGlyphs = names.split(",")
+        if ignoreGlyphs:
+            self.documentObject.lib[skipExportKey] = ignoreGlyphs
         dataElements = self.root.findall(".data")
         if not dataElements:
             return
-
         newLib = {}
+        interactionSourcesAdded = False
         for dataElement in dataElements:
             name = dataElement.attrib.get('name')
             value = dataElement.attrib.get('value')
@@ -61,20 +117,104 @@ class SuperpolatorReader(LogMixin):
                     value = float(value)
                 except ValueError:
                     pass
+            if name == "previewtext":
+                self.documentObject.lib[skateboardPreviewTextKey] = value
+            elif name == "horizontalPreviewAxis":
+                interactionSources['horizontal'].append(value)
+                interactionSourcesAdded = True
+            elif name == "verticalPreviewAxis":
+                interactionSources['vertical'].append(value)
+                interactionSourcesAdded = True
+                            
             newLib[name] = value
+        if interactionSourcesAdded:
+            self.documentObject.lib[skateboardInteractionSourcesKey] = interactionSources
         if newLib:
             self.documentObject.lib[superpolatorDataLibKey] = newLib
 
-    def readRules(self):
+
+    def readOldRules(self):
+        # read the old rules
+        # <rule enabled="1" logic="all" resultfalse="B" resulttrue="B.round">
+        #   <condition axisname="AxisWidth" operator="==" xvalue="100.000000"/>
+        # </rule>
+
+        # superpolator old rule to simple rule
+        # if op in ['<', '<=']:
+        #     # old style data
+        #     axes[axisName]['maximum'] = conditionDict['values']
+        #     newRule.name = "converted %s < and <= "%(axisName)
+        # elif op in ['>', '>=']:
+        #     # old style data
+        #     axes[axisName]['minimum'] = conditionDict['values']
+        #     newRule.name = "converted %s > and >= "%(axisName)
+        # elif op == "==":
+        #     axes[axisName]['maximum'] = conditionDict['values']
+        #     axes[axisName]['minimum'] = conditionDict['values']
+        #     newRule.name = "converted %s == "%(axisName)
+        #     newRule.enabled = False
+        # elif op == "!=":
+        #     axes[axisName]['maximum'] = conditionDict['values']
+        #     axes[axisName]['minimum'] = conditionDict['values']
+        #     newRule.name = "unsupported %s != "%(axisName)
+        #     newRule.enabled = False
+        # else:
+        #     axes[axisName]['maximum'] = conditionDict['minimum']
+        #     axes[axisName]['minimum'] = conditionDict['maximum']
+        #     newRule.name = "minmax legacy rule for %s"%axisName
+        #     newRule.enabled = False
+
+        rules = []
+        for oldRuleElement in self.root.findall(".rule"):
+            ruleObject = self.ruleDescriptorClass()
+            # only one condition set in these old rules
+            cds = []
+            a = oldRuleElement.attrib['resultfalse']
+            b = oldRuleElement.attrib['resulttrue']
+            ruleObject.subs.append((a,b))
+            for oldConditionElement in oldRuleElement.findall(".condition"):
+                cd = {}
+                operator = oldConditionElement.attrib['operator']
+                axisValue = float(oldConditionElement.attrib['xvalue'])
+                axisName = oldConditionElement.attrib['axisname']
+                if operator in ['<', '<=']:
+                    cd['maximum'] = axisValue
+                    cd['minimum'] = None
+                    cd['name'] = axisName
+                    ruleObject.name = "converted %s < and <= "%(axisName)
+                elif operator in ['>', '>=']:
+                    cd['maximum'] = None
+                    cd['minimum'] = axisValue
+                    cd['name'] = axisName
+                    ruleObject.name = "converted %s > and >= "%(axisName)
+                elif operator in ["==", "!="]:
+                    # can't convert this one
+                    continue
+                cds.append(cd)
+            if cds:
+                print("oldRuleElement", axisName, operator, oldRuleElement.attrib.items())
+                print('cds',cds)
+                ruleObject.conditionSets.append(cds)
+                self.documentObject.addRule(ruleObject)
+
+    def readSimpleRules(self):
         # read the simple rule elements
+        # <simplerules>
+        #     <simplerule enabled="1" name="width: &lt; 500.0">
+        #         <sub name="I" with="I.narrow"/>
+        #         <condition axisname="width" maximum="500"/>
+        #         <condition axisname="grade" minimum="0" maximum="500"/>
+        #     </simplerule>
+        # </simplerules>
+
+
         rulesContainerElements = self.root.findall(".simplerules")
-        print('rulesContainerElements', rulesContainerElements)
+        #print('rulesContainerElements', rulesContainerElements)
         rules = []
         for rulesContainerElement in rulesContainerElements:
             for ruleElement in rulesContainerElement:
                 ruleObject = self.ruleDescriptorClass()
                 ruleName = ruleObject.name = ruleElement.attrib['name']
-                print('rule', ruleName)
                 # subs
                 for subElement in ruleElement.findall('.sub'):
                     a = subElement.attrib['name']
@@ -85,16 +225,13 @@ class SuperpolatorReader(LogMixin):
                     ruleElement,
                     ruleName,
                 )
-                print("externalConditions", externalConditions)
                 if externalConditions:
                     ruleObject.conditionSets.append(externalConditions)
                     self.log.info(
                         "Found stray rule conditions outside a conditionset. "
                         "Wrapped them in a new conditionset."
                     )
-            print("ruleObject.name", ruleObject.name)
-            rules.append(ruleObject)
-        self.documentObject.rules = rules
+                self.documentObject.addRule(ruleObject)
 
     def _readConditionElements(self, parentElement, ruleName=None):
         # modified from the method from fonttools.designspaceLib
@@ -121,6 +258,7 @@ class SuperpolatorReader(LogMixin):
                     "condition missing required minimum or maximum in rule" +
                     (" '%s'" % ruleName if ruleName is not None else ""))
             cds.append(cd)
+        print('\t\t_readConditionElements', cds)
         return cds
 
     def readAxes(self):
@@ -128,7 +266,6 @@ class SuperpolatorReader(LogMixin):
         axisElements = self.root.findall(".axis")
         if not axisElements:
             # raise error, we need axes
-            print("nope")
             return
         for axisElement in axisElements:
             axisObject = self.axisDescriptorClass()
@@ -208,6 +345,11 @@ class SuperpolatorReader(LogMixin):
             if styleName is not None:
                 sourceObject.styleName = styleName
             sourceObject.location = self.locationFromElement(sourceElement)
+            isMuted = False
+            for maskedElement in sourceElement.findall('.maskedfont'):
+                # mute isn't stored in the sourceDescriptor, but we can store it in the lib
+                if maskedElement.attrib.get('font') == "1":
+                    isMuted = True
             for libElement in sourceElement.findall('.provideLib'):
                 if libElement.attrib.get('state') == '1':
                     sourceObject.copyLib = True
@@ -227,6 +369,11 @@ class SuperpolatorReader(LogMixin):
                 if glyphElement.attrib.get('mute') == '1':
                     sourceObject.mutedGlyphNames.append(glyphName)
             self.documentObject.sources.append(sourceObject)
+            if isMuted:
+                if not skateboardMutedSourcesKey in self.documentObject.lib:
+                    self.documentObject.lib[skateboardMutedSourcesKey] = []
+                item = (sourceObject.filename, "foreground")
+                self.documentObject.lib[skateboardMutedSourcesKey].append(item)
 
     def readInstances(self):
         for instanceCount, instanceElement in enumerate(self.root.findall(".instance")):
@@ -251,72 +398,93 @@ class SuperpolatorReader(LogMixin):
                     instanceObject.info = True
             self.documentObject.instances.append(instanceObject)
 
-if __name__ == "__main__":
-    testDoc = DesignSpaceDocument()
-    testPath = "../../Tests/spReader_testdocs/superpolator_testdoc1.sp3"
-    reader = SuperpolatorReader(testPath, testDoc)
+def sp3_to_designspace(sp3path, designspacePath=None):
+    if designspacePath is None:
+        designspacePath = sp3path.replace(".sp3", ".designspace")
+    doc = DesignSpaceDocument()
+    reader = SuperpolatorReader(sp3path, doc)
     reader.read()
-
-    # check the axes
-    names = [a.name for a in reader.documentObject.axes]
-    names.sort()
-    assert names == ['grade', 'space', 'weight', 'width']
-    tags = [a.tag for a in reader.documentObject.axes]
-    tags.sort()
-    assert tags == ['SPCE', 'grad', 'wdth', 'wght']
-
-    # check the data items
-    assert superpolatorDataLibKey in reader.documentObject.lib
-    items = list(reader.documentObject.lib[superpolatorDataLibKey].items())
-    items.sort()
-    assert items == [('expandRules', False), ('horizontalPreviewAxis', 'width'), ('includeLegacyRules', False), ('instancefolder', 'instances'), ('keepWorkFiles', True), ('lineInverted', True), ('lineStacked', 'lined'), ('lineViewFilled', True), ('outputFormatUFO', 3.0), ('previewtext', 'VA'), ('roundGeometry', False), ('verticalPreviewAxis', 'weight')]
-
-    # check the sources
-    print("reader.documentObject.sources: %d items" % len(reader.documentObject.sources))
-    for sd in reader.documentObject.sources:
-        assert sd.familyName == "MutatorMathTest_SourceFamilyName"
-        if sd.styleName == "Default":
-            assert sd.location == {'width': 0.0, 'weight': 0.0, 'space': 0.0, 'grade': -0.5}
-            assert sd.copyLib == True
-            assert sd.copyGroups == True
-            assert sd.copyInfo == True
-            assert sd.copyFeatures == True
-        elif sd.styleName == "TheOther":
-            assert sd.location == {'width': 0.0, 'weight': 1000.0, 'space': 0.0, 'grade': -0.5}
-            assert sd.copyLib == False
-            assert sd.copyGroups == False
-            assert sd.copyInfo == False
-            assert sd.copyFeatures == False
-
-    # check the instances
-    print("reader.documentObject.instances: %d items" % len(reader.documentObject.instances))
-    for nd in reader.documentObject.instances:
-        assert nd.familyName == "MutatorMathTest_InstanceFamilyName"
-        if nd.styleName == "AWeightThatILike":
-            assert nd.location == {'width': 133.152174, 'weight': 723.981097, 'space': 0.0, 'grade': -0.5}
-            assert nd.filename == "instances/MutatorMathTest_InstanceFamilyName-AWeightThatILike.ufo"
-            assert nd.styleMapFamilyName == None
-            assert nd.styleMapStyleName == None
-        if nd.styleName == "wdth759.79_SPCE0.00_wght260.72":
-            # note the anisotropic location in the width axis.
-            assert nd.location == {'width': (500.0, 800.0), 'weight': 260.7217, 'space': 0.0, 'grade': -0.5}
-            assert nd.filename == "instances/MutatorMathTest_InstanceFamilyName-wdth759.79_SPCE0.00_wght260.72.ufo"
-            assert nd.styleMapFamilyName == "StyleMappedFamily"
-            assert nd.styleMapStyleName == "bold"
-
-    # check the rules
-    for rd in reader.documentObject.rules:
-        assert rd.name == "width: < 500.0"
-        assert len(rd.conditionSets) == 1
-        assert rd.subs == [('I', 'I.narrow')]
-        for conditionSet in rd.conditionSets:
-            for cd in conditionSet:
-                print(cd)
-                if cd['name'] == "width":
-                    assert cd == {'minimum': None, 'maximum': 500.0, 'name': 'width'}
-                if cd['name'] == "grade":
-                    assert cd == {'minimum': 0.0, 'maximum': 500.0, 'name': 'grade'}
+    doc.write(designspacePath)
 
 
-    testDoc.write(testPath.replace(".sp3", "_output_roundtripped.designspace"))
+if __name__ == "__main__":
 
+    def test_superpolator_testdoc1():
+        # read superpolator_testdoc1.sp3
+        # and test all the values
+        testDoc = DesignSpaceDocument()
+        testPath = "../../Tests/spReader_testdocs/superpolator_testdoc1.sp3"
+        reader = SuperpolatorReader(testPath, testDoc)
+        reader.read()
+
+        # check the axes
+        names = [a.name for a in reader.documentObject.axes]
+        names.sort()
+        assert names == ['grade', 'space', 'weight', 'width']
+        tags = [a.tag for a in reader.documentObject.axes]
+        tags.sort()
+        assert tags == ['SPCE', 'grad', 'wdth', 'wght']
+
+        # check the data items
+        assert superpolatorDataLibKey in reader.documentObject.lib
+        items = list(reader.documentObject.lib[superpolatorDataLibKey].items())
+        items.sort()
+        assert items == [('expandRules', False), ('horizontalPreviewAxis', 'width'), ('includeLegacyRules', False), ('instancefolder', 'instances'), ('keepWorkFiles', True), ('lineInverted', True), ('lineStacked', 'lined'), ('lineViewFilled', True), ('outputFormatUFO', 3.0), ('previewtext', 'VA'), ('roundGeometry', False), ('verticalPreviewAxis', 'weight')]
+
+        # check the sources
+        print("reader.documentObject.sources: %d items" % len(reader.documentObject.sources))
+        for sd in reader.documentObject.sources:
+            assert sd.familyName == "MutatorMathTest_SourceFamilyName"
+            if sd.styleName == "Default":
+                assert sd.location == {'width': 0.0, 'weight': 0.0, 'space': 0.0, 'grade': -0.5}
+                assert sd.copyLib == True
+                assert sd.copyGroups == True
+                assert sd.copyInfo == True
+                assert sd.copyFeatures == True
+            elif sd.styleName == "TheOther":
+                assert sd.location == {'width': 0.0, 'weight': 1000.0, 'space': 0.0, 'grade': -0.5}
+                assert sd.copyLib == False
+                assert sd.copyGroups == False
+                assert sd.copyInfo == False
+                assert sd.copyFeatures == False
+
+        # check the instances
+        print("reader.documentObject.instances: %d items" % len(reader.documentObject.instances))
+        for nd in reader.documentObject.instances:
+            assert nd.familyName == "MutatorMathTest_InstanceFamilyName"
+            if nd.styleName == "AWeightThatILike":
+                assert nd.location == {'width': 133.152174, 'weight': 723.981097, 'space': 0.0, 'grade': -0.5}
+                assert nd.filename == "instances/MutatorMathTest_InstanceFamilyName-AWeightThatILike.ufo"
+                assert nd.styleMapFamilyName == None
+                assert nd.styleMapStyleName == None
+            if nd.styleName == "wdth759.79_SPCE0.00_wght260.72":
+                # note the anisotropic location in the width axis.
+                assert nd.location == {'width': (500.0, 800.0), 'weight': 260.7217, 'space': 0.0, 'grade': -0.5}
+                assert nd.filename == "instances/MutatorMathTest_InstanceFamilyName-wdth759.79_SPCE0.00_wght260.72.ufo"
+                assert nd.styleMapFamilyName == "StyleMappedFamily"
+                assert nd.styleMapStyleName == "bold"
+
+        # check the rules
+        for rd in reader.documentObject.rules:
+            assert rd.name == "width: < 500.0"
+            assert len(rd.conditionSets) == 1
+            assert rd.subs == [('I', 'I.narrow')]
+            for conditionSet in rd.conditionSets:
+                for cd in conditionSet:
+                    if cd['name'] == "width":
+                        assert cd == {'minimum': None, 'maximum': 500.0, 'name': 'width'}
+                    if cd['name'] == "grade":
+                        assert cd == {'minimum': 0.0, 'maximum': 500.0, 'name': 'grade'}
+
+
+        testDoc.write(testPath.replace(".sp3", "_output_roundtripped.designspace"))
+
+    def test_testDocs():
+        # read the test files and convert them
+        # no tests
+        root = "../../Tests/spReader_testdocs/test*.sp3"
+        for path in glob.glob(root):
+            print("----", path)
+            sp3_to_designspace(path)
+
+    test_testDocs()
