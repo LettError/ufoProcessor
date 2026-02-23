1af0bc3f7de3a503b5f04c9143ad30f144efa050
Erik van Blokland <erik@letterror.com>
2026-02-23 11:23:34 +0100

    Use RecordingPointPen for the intermediate glyph

--- Lib/ufoProcessor/rules.py
+++ Lib/ufoProcessor/rules.py
@@ -1,72 +1,40 @@
+from fontTools.pens.recordingPen import RecordingPointPen
 
-
-def swapGlyphNames(font, oldName, newName, swapNameExtension = "_______________swap"):
+def swapGlyphNames(font, oldName, newName):
     # In font swap the glyphs oldName and newName.
     # Also swap the names in components in order to preserve appearance.
     # Also swap the names in font groups.
     if not oldName in font or not newName in font:
         return None
-    swapName = oldName + swapNameExtension
-    # park the old glyph
-    if not swapName in font:
-        font.newGlyph(swapName)
-    # get anchors
-    oldAnchors = font[oldName].anchors
-    newAnchors = font[newName].anchors
+    
+    oldRecording = RecordingPointPen()
+    newRecording = RecordingPointPen()
+    
+    oldGlyph = font[oldName]
+    newGlyph = font[newName]
+    
+    oldGlyph.drawPoints(oldRecording)
+    newGlyph.drawPoints(newRecording)
+    
+    # swap widths    
+    oldGlyph.width, newGlyph.width = newGlyph.width, oldGlyph.width
 
-    # swap the outlines
-    font[swapName].clear()
-    p = font[swapName].getPointPen()
-    font[oldName].drawPoints(p)
-    font[swapName].width = font[oldName].width
-    # lib?
-    font[oldName].clear()
-    p = font[oldName].getPointPen()
-    font[newName].drawPoints(p)
-    font[oldName].width = font[newName].width
-    for a in newAnchors:
-        na = defcon.Anchor()
-        na.name = a.name
-        na.x = a.x
-        na.y = a.y
-        # FontParts and Defcon add anchors in different ways
-        # this works around that.
-        try:
-            font[oldName].naked().appendAnchor(na)
-        except AttributeError:
-            font[oldName].appendAnchor(na)
-
-    font[newName].clear()
-    p = font[newName].getPointPen()
-    font[swapName].drawPoints(p)
-    font[newName].width = font[swapName].width
-    for a in oldAnchors:
-        na = defcon.Anchor()
-        na.name = a.name
-        na.x = a.x
-        na.y = a.y
-        try:
-            font[newName].naked().appendAnchor(na)
-        except AttributeError:
-            font[newName].appendAnchor(na)
-
+    # swap the glyph objects
+    oldGlyph.clear()
+    newGlyph.clear()
+    
+    oldRecording.replay(newGlyph.getPointPen())
+    newRecording.replay(oldGlyph.getPointPen())
 
     # remap the components
     for g in font:
         for c in g.components:
            if c.baseGlyph == oldName:
-               c.baseGlyph = swapName
-           continue
-    for g in font:
-        for c in g.components:
-           if c.baseGlyph == newName:
-               c.baseGlyph = oldName
-           continue
-    for g in font:
-        for c in g.components:
-           if c.baseGlyph == swapName:
                c.baseGlyph = newName
-
+           elif c.baseGlyph == newName:
+               c.baseGlyph = oldName
+        g.changed()
+    
     # change the names in groups
     # the shapes will swap, that will invalidate the kerning
     # so the names need to swap in the kerning as well.
@@ -96,10 +64,7 @@ def swapGlyphNames(font, oldName, newName, swapNameExtension = "_______________s
                 newMembers.append(name)
         font.groups[groupName] = newMembers
 
-    remove = []
-    for g in font:
-        if g.name.find(swapNameExtension)!=-1:
-            remove.append(g.name)
-    for r in remove:
-        del font[r]
-
+    
+    
+#swapGlyphNames(CurrentFont(), "B", "B.alt")
+#swapGlyphNames(CurrentFont(), "B.alt", "B")
